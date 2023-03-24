c5237f1af1d1597e514946ca98987e127c770fc6
daraffleur <romaniuk.dana@gmail.com>
2023-03-24 10:29:30 +0200

    [A]: db and bot logic

--- app/views/main.py
+++ app/views/main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 from flask import render_template, request, Blueprint, flash, redirect
 
@@ -8,24 +9,25 @@ from app.logger import log
 
 
 main_blueprint = Blueprint("main", __name__)
-EMAIL_ENDPOINT = os.environ.get("EMAIL_ENDPOINT")
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
 
-# def send_applicant_to_telegram_chat(text):
-#     method = "sendMessage"
-#     token = "2049371679:AAGLoC9AaUdNAaxrsgCYOEF15kiXZmRVTq8"
-#     chat_id = "-1001398146536"
-#     url = f"https://api.telegram.org/bot{token}/{method}"
-#     data = {"chat_id": chat_id, "text": text}
-#     requests.post(url, data=data)
+def send_applicant_to_telegram_chat(text):
+    method = "sendMessage"
+    token = TELEGRAM_TOKEN
+    chat_id = TELEGRAM_CHAT_ID
+    url = f"https://api.telegram.org/bot{token}/{method}"
+    data = {"chat_id": chat_id, "text": text}
+    requests.post(url, data=data)
 
 
 @main_blueprint.route("/", methods=["GET", "POST"])
 def index():
     form = ApplicantForm(request.form)
     if form.validate_on_submit():
-        # applicant = f"НОВА ЗАЯВКА НА КУРС\n\nПрізвище: {form.first_name.data}\nІм'я: {form.last_name.data}\n \nЕмейл: {form.email.data}\nНомер телефону: {form.phone.data}"
-        # send_applicant_to_telegram_chat(applicant)
+        applicant = f"NEW APPLICATION \n\nFirst Name: {form.first_name.data}\nLast Name: {form.last_name.data}\n \nEmail: {form.email.data}\nPhone Number: {form.phone.data}"
+        send_applicant_to_telegram_chat(applicant)
         applicant = Applicant(
             first_name=form.first_name.data,
             last_name=form.last_name.data,
@@ -41,8 +43,9 @@ def index():
             log.DEBUG,
             "Your application has been sent successfully! We will call you soon!",
         )
-        # return redirect("https://tokarstudy.com.ua/#registration")
-        return redirect("http://46.101.167.8/#registration")
+        # return redirect("http://46.101.167.8/#registration")
+        return redirect("http://127.0.0.1:5000/#registration")
+    
     elif form.is_submitted():
         for error in form.errors:
             for msg in form.errors[error]:
@@ -51,6 +54,6 @@ def index():
                     "Something went wrong... Please reload the page and try submitting an application again. ",
                     "danger",
                 )
-                # return redirect("https://tokarstudy.com.ua/#registration")
-                return redirect("http://46.101.167.8/#registration")
+                return redirect("http://127.0.0.1:5000/#registration")
+                # return redirect("http://46.101.167.8/#registration")
     return render_template("index.html", form=form)
